7305b4acd35ee6a1138daea5b590edb95d30185b
Yash Siwach <112823140+yashsiwach@users.noreply.github.com>
2024-06-26 05:15:06 +0530

    ok

--- app.js
+++ app.js
@@ -1,25 +1,48 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+//react element
 const heading = React.createElement(
     "h1",
     { id: "heading" },
     "hello ram ram"
 );
-const parent = React.createElement(
-    "div",
-    { id: "parent" },
-    [React.createElement(
-        "h1",
-        { id: "child" },
-        "hi child",
-    ),
-    React.createElement(
-        "h2",
-        { id: "child" },
-        "hi2 child",
-    )]
+console.log(heading);
 
-)
-console.log(parent);
+const jsx = (
+    <h1 id="test" className="ok">
+        hello from jsx
+    </h1>
+);
+// react components -> functional components
+const Functional = () => {
+    return (
+        <div id="container">
+            <h1>hello from jaat</h1>
+        </div>
+    );
+};
+
+const test = 199;
+
+const Big = () => {
+    return (
+        <div id="container">
+            {/* work as a function ,component in component is component composition */}
+            <Functional />
+            {Functional()}
+            {test + 100}
+            {console.log("print ook")}
+            {/* {//element inside component  */}
+
+            {jsx}
+            <h1>hello from jaat</h1>
+        </div>
+    );
+};
+
+const Functional2 = () => <h1>hello from jaat</h1>;
+
+console.log(Functional());
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent);
+root.render(<Big />);
+
